72f154e534a7c1f61b539f8bed2e4c74fef6f0ec
tarikdavis <tarikdavis@hotmail.co.uk>
2025-07-29 16:24:41 +0100

    final commit, now on to the real site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 export default defineConfig({
-  site: "https://jade-meerkat-7fac5b.netlify.app/"
+  site: "https://jade-meerkat-7fac5b.netlify.app/",
+  integrations: [preact()]
 });
